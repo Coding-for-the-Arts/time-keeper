5e9ae478d2b77fe0450dc46d3bc3c08b92c00530
Coding for the Arts <88927553+cas-c4ta@users.noreply.github.com>
2021-10-18 13:37:44 +0200

    fix path

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+/*
+  Mit Sinus angetriebene Animationen
+*/
+
+let a = 0 // «Winkel» zur Errechnung von Sinus/Cosinus
+
 function setup() {
+  createCanvas(400, 400)
 
 }
 
 function draw() {
+  let factor = sin(a)
 
+  background(220)
+
+  /* Ein Kreis von links nach rechts */
+  let diameter = 10
+  let circleX = map(factor, -1, 1, 0, width-diameter)
+  ellipse(circleX, 100, diameter)
+
+  // Den «Winkel» erhöhen
+  const increment = TAU/360
+  a += increment
 }
